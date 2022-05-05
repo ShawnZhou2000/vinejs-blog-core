43effb712d7f23a3ff71c6ed0b1f98f9751fa21c
Shawn Zhou <shawnzhou101@163.com>
2022-05-05 08:07:37 +0800

    feat: beian

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="blog-core__footer-container">
-    <p>Copyrights © {{ start }} {{ owner }}</p>
+    <p>Copyrights © {{ siteTime }} {{ owner }}</p>
+    <p v-if="beian?.enable">
+      <a :href="beian.icp.url" target="_blank" class="blog-core__icp">{{ beian.icp.text }}</a>
+      <a :href="beian.police.url" target="_blank" class="blog-core__police">{{ beian.police.text }}</a>
+    </p>
     <p>由 Vine.js 强力驱动 | 主核心 - blog-core:{{theme}}</p>    
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, toRef } from 'vue'
+import { defineComponent, toRef, computed, Ref } from 'vue'
 
 export default defineComponent({
   props: ['owner', 'start', 'beian', 'theme'],
   setup(props, ctx) {
+    const start: Ref<number> = toRef(props, 'start');
+    const now: number = new Date().getFullYear();
+    const siteTime = computed(() => {
+      if (now === start.value) {
+        return start.value;
+      } else {
+        return `${start.value} - ${now}`;
+      }
+    })
     return {
       owner: toRef(props, 'owner'),
-      start: toRef(props, 'start'),
       beian: toRef(props, 'beian'),
       theme: toRef(props, 'theme'),
+      siteTime
     }
   },
 })
@@ -34,5 +47,29 @@ export default defineComponent({
       margin-top: 0;
       margin-bottom: 0.5rem;
     }
+  }
+
+  .blog-core__icp, .blog-core__police {
+    color: rgba(0,0,0,0.9);
+    text-decoration: none;
+    transition: all 0.1s ease;
+    &:hover {
+      color: $text-highlight;
+    }
+  }
+  .blog-core__icp {
+    margin-right: 2rem;
+  }
+  .blog-core__police {
+    position: relative;
+  }
+  .blog-core__police::before {
+    content: "";
+    width: 1rem;
+    height: 1rem;
+    background: url('/beian.png');
+    background-size: contain;
+    position: absolute;
+    left: -1.2rem;
   }
 </style>
